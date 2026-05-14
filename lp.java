334a077a6ed4a51cc8ee1870b5a07aeb5de07226
nupurchaudharip12-afk <nupurchaudharip12@gmail.com>
2026-05-15 01:39:28 +0530

    Add GreetingClass with sayHello method

---
+++ lp.java
@@ -0,0 +1,8 @@
+public class GreetingClass {
+    // A simple method to return a greeting
+    public static String sayHello(String name) {
+        String message = 'Hello, ' + name + '!';
+        System.debug(message); // Prints to the debug log
+        return message;
+    }
+}
